c53e2088ac29c252ce0dc9731b702c61bc6961a2
Nipun Theekshana <theekshana2@gmail.com>
2024-08-04 00:07:22 +0200

    fix: load env error

--- main.go
+++ main.go
@@ -14,10 +14,7 @@ import (
 )
 
 func main() {
-    err := godotenv.Load()
-    if err != nil {
-        fmt.Println("Error loading .env file")
-    }
+	_ = godotenv.Load()
 
     app := fiber.New()
 
